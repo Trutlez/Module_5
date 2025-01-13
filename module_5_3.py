a5911fb17e1eed06946ce555c274af64fa436098
Trutlez <orppo@yandex.ru>
2025-01-13 17:33:32 +0500

    Задача "Нужно больше этажей"

---
+++ module_5_3.py
@@ -0,0 +1,90 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def go_to(self, new_floor):
+        if self.number_of_floors < new_floor:
+            print('Такого этажа не существует')
+        else:
+            for i in range(new_floor):
+                print(i + 1)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        else:
+            print(f'Аргумент {value} не является ни домом, ни целым числом')
+        return self
+
+    def __radd__(self, value):
+        return self + value
+
+    def __iadd__(self, value):
+        return self + value
+
+    def __mul__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors *= value
+        elif isinstance(value, House):
+            self.number_of_floors *= value.number_of_floors
+        else:
+            print(f'Аргумент {value} не является ни домом, ни целым числом')
+        return self
+
+    def __rmul__(self, value):
+        return self * value
+
+    def __imul__(self, value):
+        return self * value
+
+# Пример результата выполнения программы:
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
